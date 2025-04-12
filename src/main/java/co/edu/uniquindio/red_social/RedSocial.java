7eee1738fb260a8479de6a52d85e4f2e3f227e06
helengiraldo <helen8335@gmail.com>
2025-04-12 16:08:12 -0500

    Diseño de interfaz de perfil y botón de cerrar sesión

--- src/main/java/co/edu/uniquindio/red_social/RedSocial.java
+++ src/main/java/co/edu/uniquindio/red_social/RedSocial.java
@@ -10,11 +10,12 @@ import java.io.IOException;
 public class RedSocial extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(RedSocial.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        FXMLLoader fxmlLoader = new FXMLLoader(RedSocial.class.getResource("mensajes.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("Super Class");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module co.edu.uniquindio.red_social {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens co.edu.uniquindio.red_social to javafx.fxml;
