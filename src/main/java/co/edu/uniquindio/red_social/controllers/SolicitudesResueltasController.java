cfb87a964b5564091610bcf1feef536771934d8b
helengiraldo <helen8335@gmail.com>
2025-05-25 12:07:10 -0500

    interfaz de solicitudesResueltas

---
+++ src/main/java/co/edu/uniquindio/red_social/controllers/SolicitudesResueltasController.java
@@ -0,0 +1,85 @@
+package co.edu.uniquindio.red_social.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class SolicitudesResueltasController {
+
+    @FXML
+    private VBox AnchorPaneContenedorSolicitudesResueltas;
+
+    @FXML
+    private Label ContenidoSolicitudLabel;
+
+    @FXML
+    private HBox HboxSolicitud;
+
+    @FXML
+    private HBox HboxSolicitud2;
+
+    @FXML
+    private Label TextFieldTitle;
+
+    @FXML
+    private Button botonBuscar;
+
+    @FXML
+    private TextField campoBusqueda;
+
+    @FXML
+    private ComboBox<?> comboFiltro;
+
+    @FXML
+    private AnchorPane contenedorSolicitudes;
+
+    @FXML
+    private Label contenidoSolicitudLabel2;
+
+    @FXML
+    private Label filtrarSolicitudesLabel;
+
+    @FXML
+    private Label handleVolver;
+
+    @FXML
+    private ImageView imagenPerfil;
+
+    @FXML
+    private Label importancia2Label;
+
+    @FXML
+    private Label importanciaLabel;
+
+    @FXML
+    private Label nombreEstudiante2Label;
+
+    @FXML
+    private Label nombreEstudianteLabel;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private Label solicitudesAyudaLabel;
+
+    @FXML
+    private Label solucionLabel;
+
+    @FXML
+    private Label solucionLabel2;
+
+    @FXML
+    private Label titulo2;
+
+}
