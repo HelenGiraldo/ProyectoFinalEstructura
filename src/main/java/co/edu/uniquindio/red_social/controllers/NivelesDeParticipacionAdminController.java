32fd013c77e1c8af5a5b33abdef9d934e90fbcaa
helengiraldo <helen8335@gmail.com>
2025-05-27 17:39:23 -0500

    interfaces estadísticas con controller de niveles de participacion

---
+++ src/main/java/co/edu/uniquindio/red_social/controllers/NivelesDeParticipacionAdminController.java
@@ -0,0 +1,94 @@
+package co.edu.uniquindio.red_social.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class NivelesDeParticipacionAdminController {
+
+    @FXML
+    private Label IdLabel;
+
+    @FXML
+    private Label TextFieldTitle;
+
+    @FXML
+    private Label amigosLabel;
+
+    @FXML
+    private Button buttonCaminoMasCorto;
+
+    @FXML
+    private Button buttonComunidades;
+
+    @FXML
+    private Button buttonMasConexiones;
+
+    @FXML
+    private Button buttonMasValorados;
+
+    @FXML
+    private VBox chatListVBoxFondoDos;
+
+    @FXML
+    private VBox chatSpace;
+
+    @FXML
+    private ComboBox<?> comboBoxEstudiante;
+
+    @FXML
+    private Label gruposLabel;
+
+    @FXML
+    private Label handlevolver;
+
+    @FXML
+    private Label nombreLabel;
+
+    @FXML
+    private Label publicacionesLabel;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private ScrollPane scrollPaneContenedorContenidos;
+
+    @FXML
+    private ScrollPane scrollPrincipal;
+
+    @FXML
+    private Label valoracionesLabel;
+
+    @FXML
+    void handleVolver(MouseEvent event) {
+
+    }
+
+    @FXML
+    void irACaminoMasCorto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irADeteccionComunidades(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irAEstudiantesMasConexiones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irAMasValorados(ActionEvent event) {
+
+    }
+
+}
