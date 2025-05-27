ef4a508bbe2c89e24269c8e682a670f8ae5516d1
helengiraldo <helen8335@gmail.com>
2025-05-27 16:06:54 -0500

    contenidosMasValorados.fxml y ContenidosMasValoradosAdminController

---
+++ src/main/java/co/edu/uniquindio/red_social/controllers/ContenidosMasValoradosAdminController.java
@@ -0,0 +1,85 @@
+package co.edu.uniquindio.red_social.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class ContenidosMasValoradosAdminController {
+
+    @FXML
+    private Label TextFieldTitle;
+
+    @FXML
+    private TableColumn<?, ?> autorTableColumun;
+
+    @FXML
+    private Button buttonCaminoCorto;
+
+    @FXML
+    private Button buttonComunidades;
+
+    @FXML
+    private Button buttonMasConexiones;
+
+    @FXML
+    private Button buttonParticipacion;
+
+    @FXML
+    private VBox chatListVBoxFondoDos;
+
+    @FXML
+    private VBox chatSpace;
+
+    @FXML
+    private TableColumn<?, ?> contenidoTableColumun;
+
+    @FXML
+    private Label contenidosLabel;
+
+    @FXML
+    private TableColumn<?, ?> grupoTableColumun;
+
+    @FXML
+    private Label handlevolver;
+
+    @FXML
+    private Label masValoradosLabel;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private ScrollPane scrollPaneContenedorContenidos;
+
+    @FXML
+    private ScrollPane scrollPrincipal;
+
+    @FXML
+    private TableColumn<?, ?> valoracionTableColumun;
+
+    @FXML
+    void irACaminoMasCorto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irADeteccionComunidades(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irAMasConexiones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irANivelesParticipacion(ActionEvent event) {
+
+    }
+
+}
