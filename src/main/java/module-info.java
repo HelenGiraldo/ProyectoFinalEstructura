51ef59520da49809f45d540d8a6109c115a04550
helengiraldo <helen8335@gmail.com>
2025-05-28 00:28:46 -0500

    pruebas unitarias

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module co.edu.uniquindio.red_social {
     requires jdk.jshell;
 
 
+
     opens co.edu.uniquindio.red_social to javafx.fxml;
     exports co.edu.uniquindio.red_social;
     exports co.edu.uniquindio.red_social.controllers;
